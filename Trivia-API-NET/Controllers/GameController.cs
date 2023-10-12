0105cb596f8c81926cea3b103aac3a79b0c7cf15
Andrew Thiele <308butterfly@gmail.com>
2023-10-12 09:13:17 -0500

    Add project files.

---
+++ Trivia-API-NET/Controllers/GameController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+
+namespace Trivia_API_NET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController : ControllerBase
+    {
+        public GameController() { }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetGameQuestions([FromQuery] int count, int categoryId, string difficulty, string type) {
+
+            var baseURL = "https://opentdb.com/api.php";
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var results = await client.GetAsync();
+
+
+                }
+                catch {
+                    return BadRequest();
+                }
+            }
+            return Ok("Game Questions retrieved");
+        }
+    }
+}
---
+++ Trivia-API-NET/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Singleton, Scoped, Transient
+/*
+ Singleton - Service for lifetime of the application
+ Scoped - created once per client request i.e. sql connection
+ Transient - use it and throw it away like an httpClient
+ */
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
